018ce36723b43dcc8e12cbf844712d6a63bd0ecf
Christian Lazo <lazochristian.cl@gmail.com>
2017-10-19 20:45:12 -0700

    Added create menu link to homepage

--- spec/features/static_spec.rb
+++ spec/features/static_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 
 describe 'navigate' do
   describe 'homepage' do
-    it 'can be reached successfully' do
+    before do
       visit root_path
+    end
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
+    end
+
+    it 'has a link to create menu' do
+      find_link('Create Menu Item')
     end
   end
 end
