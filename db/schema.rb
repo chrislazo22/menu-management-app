0b30cf6e1771eb0e93a4b3962ac9adbf63a5d9a6
Christian Lazo <lazochristian.cl@gmail.com>
2017-10-21 12:48:59 -0700

    Change price column to include precision and scale

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171021194036) do
+ActiveRecord::Schema.define(version: 20171021194642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20171021194036) do
     t.string "name"
     t.text "description"
     t.string "category"
-    t.decimal "price"
+    t.decimal "price", precision: 8, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
