8089c58c8e1790274fa40d13b0101e1f73c67a19
Christian Lazo <lazochristian.cl@gmail.com>
2017-10-22 13:27:45 -0700

    Create order items controller. Add price methods to order and order_items models

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,4 +1,6 @@
 class MenuItem < ApplicationRecord
+  has_many :order_items
+  
   validates_presence_of :name, :description
   validates :price, presence: true,
             numericality: true
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,2 +1,29 @@
 class OrderItem < ApplicationRecord
+  belongs_to :order
+  belongs_to :menu_item
+
+  before_save :set_unit_price
+  before_save  :set_total_price
+
+  def unit_price
+    if persisted? # returns true if it is not a new record and was not destroyed
+      self[:unit_price]
+    else
+      menu_item.price
+    end
+  end
+
+  def total_price # total price for menu_item in order
+    unit_price * quantity
+  end
+
+  private
+
+  def set_unit_price
+    self[:unit_price] = unit_price
+  end
+
+  def set_total_price
+    self[:total_price] = quantity * set_unit_price
+  end
 end
