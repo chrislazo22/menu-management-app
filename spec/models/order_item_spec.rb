f680dff029f409e42d77f45d22a5c8593b758d2a
Christian Lazo <lazochristian.cl@gmail.com>
2017-10-23 22:49:48 -0700

    Spec for testing order items model

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @menu_item = MenuItem.create name: "Pancake", description: "This is a pancake", category: "Entree", price: 10.00, id: 1
+    @order_item = OrderItem.create id: 1, menu_item_id:1, order_id: 1, unit_price: 10.00, quantity: 2
+  end
+
+  describe '#unit_price' do
+    it 'displays correct unit price' do
+      expect(@order_item.unit_price).to eq(10.00)
+    end
+  end
+
+  describe '#total_price' do
+    it 'outputs correct total_price' do
+      expect(@order_item.total_price).to eq(20.00)
+    end
+  end
 end
