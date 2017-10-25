bace2bde805e68254848d996eef9ce183f119506
Christian Lazo <lazochristian.cl@gmail.com>
2017-10-25 16:56:11 -0700

    Change image size on static page

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,10 +1,10 @@
 class MenuItem < ApplicationRecord
   has_many :order_items
 
-  has_attached_file :image, styles: { small: "100x100" }
+  has_attached_file :image, styles: { small: "150x150" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-  validates_presence_of :name, :description, :category 
+  validates_presence_of :name, :description, :category
   validates :price, presence: true,
             numericality: true
 end
