c3c3eb0b181a313027dfe7ca2cfa095fb41f69ac
Christian Lazo <lazochristian.cl@gmail.com>
2017-10-24 20:26:38 -0700

    Add table for menu items index view

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -5,4 +5,11 @@ describe 'admin page' do
     visit root_path
     expect(page).to have_content("Admin")
   end
+
+  it 'links to menu items index page' do
+    visit root_path
+    click_link('Admin')
+
+    expect(page).to have_content('Menu Items')
+  end
 end
