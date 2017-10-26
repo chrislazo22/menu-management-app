41bdfefe90e71c2855b38d4172a1de985fd48836
Christian Lazo <lazochristian.cl@gmail.com>
2017-10-25 22:06:31 -0700

    Changes to seed file to ship

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,26 +4,26 @@ OrderItem.delete_all
 User.delete_all
 
 MenuItem.create! id: 1,
-                  name: "Pancake",
+                  name: "Mozzarella Sticks",
                   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
                   category: "Appetizer",
-                  price: 3.00
+                  price: 5.00
 MenuItem.create! id: 2,
-                  name: "Waffles",
+                  name: "Seared Ahi Tuna Steak",
                   description: "Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate",
                   category: "Main Course",
-                  price: 5.00
+                  price: 10.00
 MenuItem.create! id: 3,
-                  name: "Eggs",
+                  name: "Tater Tots",
                   description: "Velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt",
                   category: "Side Dish",
-                  price: 2.00
+                  price: 7.00
 
 MenuItem.create! id: 4,
-                  name: "Bacon",
+                  name: "Cheese Cake",
                   description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae",
                   category: "Dessert",
-                  price: 6.00
+                  price: 3.00
 
 User.create!(email: "admin@admin.com", password: "asdfasdf", password_confirmation: "asdfasdf")
 
