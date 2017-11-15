d6927104f7d84feb5c765b9b13be349d9d2b54d4
Christian Lazo <lazochristian.cl@gmail.com>
2017-11-14 16:58:04 -0800

    Removed IDs from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,24 +3,22 @@ Order.delete_all
 OrderItem.delete_all
 User.delete_all
 
-MenuItem.create! id: 1,
-                  name: "Mozzarella Sticks",
+MenuItem.create!  name: "Mozzarella Sticks",
                   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
                   category: "Appetizer",
                   price: 5.00
-MenuItem.create! id: 2,
-                  name: "Seared Ahi Tuna Steak",
+
+MenuItem.create!  name: "Seared Ahi Tuna Steak",
                   description: "Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate",
                   category: "Main Course",
                   price: 10.00
-MenuItem.create! id: 3,
-                  name: "Tater Tots",
+
+MenuItem.create!  name: "Tater Tots",
                   description: "Velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt",
                   category: "Side Dish",
                   price: 7.00
 
-MenuItem.create! id: 4,
-                  name: "Cheese Cake",
+MenuItem.create!  name: "Cheese Cake",
                   description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae",
                   category: "Dessert",
                   price: 3.00
